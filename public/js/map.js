efdde72d2c193e8fb19c7ebf5097a3972d0ee1d0
Roman <romnempire@gmail.com>
2014-11-10 00:22:21 -0500

    added toybox interactivity.  it's draggable and toys can be put in and out

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,8 @@
 function dropInImage(url) {
     $('#map').append('<img src=' + url + ' class="toy draggable">');
-    $(function () {
-        $(".draggable").draggable({ containment: "parent" })
-    });
+    $('.draggable').draggable({ containment: 'parent', start: handleDragStart,
+        stop: handleDragStop, drag: handleDragDrag });
+    $('draggable').on()
     $('#urlchooser').remove();
 }
 
@@ -19,3 +19,51 @@ function createURLChooser(_this) {
         $('#urlchooser').remove();
     }
 }
+
+//the 40 is due to the h2 padding oh god kill me
+//i swaer to you this was as miserable to write as it was for you to read
+function handleDropEvent( e, ui ) {
+    var draggable = ui.draggable;
+
+    if($(this).attr('id') === 'toybox') {
+        draggable.css({left: draggable.position().left - $('#toybox').position().left, 
+            top: draggable.position().top - $('#toybox').position().top - $('#toybox > h2').height() - 40});
+    } else if ($(this).attr('id') === 'map') {
+        draggable.css({left: draggable.position().left + $('#toybox').position().left,
+            top: draggable.position().top});
+    } else {
+        draggable.css({left: '0px', top:'0px'});
+    }
+    $(this).append(draggable);
+    //draggable.draggable({ containment: 'parent'});
+}
+
+function handleDragStart( e, ui ) {
+    if($(this).parent().attr('id') === 'map'){
+        $( "#map" ).droppable({ disabled: true });
+    } else {
+        console.log($(this).parent().attr('id'));
+    }
+
+    if($(this).parent().attr('id') === 'toybox'){
+        $( "#map, #toybox" ).droppable({ disabled: true });
+    } 
+}
+
+function handleDragDrag( e, ui ) {
+    //ohh this is hacky.  super hacky.
+    if($(this).parent().attr('id') === 'toybox'){
+        if((Math.abs(ui.position.top) > $('#toybox').height()) ||
+            (Math.abs(ui.position.left) > $('#toybox').width())) {
+            $( "#map" ).droppable({ disabled:false });
+        }
+    }
+}
+
+function handleDragStop( e, ui ) {
+    $( '#map, #toybox' ).droppable({ disabled: false });
+}
+
+$( '#map, #toybox' ).droppable({ accept: ".toy", drop: handleDropEvent });
+$( '#toybox').draggable( {containment: 'parent'});
+
